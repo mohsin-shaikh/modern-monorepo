111fe44d0fcea0b2acdbcc98810d1c48dc8d9697
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2025-04-06 00:37:19 +0530

    Chore(deps): update pnpm-lock.yaml to add Radix UI components and remove obsolete dashboard pages

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -1,30 +1,40 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from 'next/font/google';
 
-import "@pkg/ui/globals.css"
-import { Providers } from "@/components/providers"
+import '@pkg/ui/globals.css';
+import { Providers } from '@/components/providers';
+import { ActiveThemeProvider } from '@/components/active-theme';
+import { cookies } from 'next/headers';
 
 const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies()
+  const activeThemeValue = cookieStore.get("active_theme")?.value
+  const isScaled = activeThemeValue?.endsWith("-scaled")
+
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <ActiveThemeProvider initialTheme={activeThemeValue}>
+            {children}
+          </ActiveThemeProvider>
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
