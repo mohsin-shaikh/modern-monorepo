8095bf44d75c9ec6837e400423522c03535f1df7
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2025-04-20 17:14:50 +0530

    Chore(cleanup): remove temporary build files
    - Deleted temporary build files and artifacts from the dashboard trigger directories to streamline the codebase.
    - Ensured that obsolete files do not clutter the project and maintain a clean working environment.

--- apps/dashboard/.trigger/tmp/build-aMOM7t/server.node-EZX3FSYY.mjs
+++
@@ -1,6 +0,0 @@
-import {
-  require_server_node
-} from "./chunk-JMNXXFTJ.mjs";
-import "./chunk-LL4QQ7FC.mjs";
-export default require_server_node();
-//# sourceMappingURL=server.node-EZX3FSYY.mjs.map
